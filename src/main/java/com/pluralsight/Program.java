d13f0643b62c5519790c298b16136543bc5a6930
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-10-29 16:15:55 -0400

    asset arraylist with loops

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -7,18 +7,17 @@ public class Program {
 
         ArrayList<Asset> assets = new ArrayList<>();
         House house = new House("Romell House", "12-10-2026", 00.00, "4585", 1, 90, 90);
-        Vehicle vehicle = new Vehicle("Sedan,silver, 4-door","2010-05-15",20000.0,"Honda Accord",2003,120000);
+        Vehicle vehicle = new Vehicle("Sedan,silver, 4-door", "2010-05-15", 20000.0, "Honda Accord", 2003, 120000);
 
 
         assets.add(house);
         assets.add(vehicle);
 
 
-        for (Asset asset: assets) {
+        for (Asset asset : assets) {
             System.out.println(asset);
             System.out.println();
         }
-
 
     }
 }
